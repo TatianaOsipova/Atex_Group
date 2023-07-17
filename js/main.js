58b3d5fa0eb9daa8b3e43a8c0ba34cbc30cd6fa8
Tatiana Osipova <otv120780@gmail.com>
2023-07-17 11:11:48 -0400

    feat: change path to image-bg

--- js/main.js
+++ js/main.js
@@ -46,8 +46,8 @@ addEventListener("resize", (event)=> {
     resizeTextToFit();
 });
 
-$(function(){
-    $('.menu__btn').on('click', function(){
-		$('.menu__list').toggleClass('menu__list--active');
-	});
-});
+// $(function(){
+//     $('.menu__btn').on('click', function(){
+// 		$('.menu__list').toggleClass('menu__list--active');
+// 	});
+// });
