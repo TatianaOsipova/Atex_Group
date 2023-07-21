1707c8f69e39fb028136f2d60c1ad5052c215dab
Tatiana Osipova <otv120780@gmail.com>
2023-07-21 13:10:03 -0400

    fix: delete previous settings of slider

--- js/main.js
+++ js/main.js
@@ -11,68 +11,17 @@ const swiper = new Swiper(".swiper", {
         slideShadows: true,
     },
     loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
     },
 });
 
-// const swiper = new Swiper(".swiper-carousel", {
-//     slidesPerView: 3,
-//     centeredSlides: true,
-//     spaceBetween: 10,
-//     autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//     },
-//     keyboard: {
-//         enabled: true,
-//     },
-//     loop: true,
-//     pagination: {
-//         el: ".swiper-pagination",
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
-
-// const slides = document.getElementsByClassName("swiper-slide");
-// for (const slide of slides) {
-//     slide.addEventListener("click", () => {
-//         const {
-//             className
-//         } = slide;
-//         if (className.includes("swiper-slide-next")) {
-//             swiper.slideNext();
-//         } else if (className.includes("swiper-slide-prev")) {
-//             swiper.slidePrev();
-//         }
-//     });
-// }
-
-// function resizeTextToFit() {
-//     const descrEls = document.getElementsByClassName('description');
-//     for (const el of descrEls) {
-//         el.style.width = el.offsetWidth;
-//         el.style.height = el.offsetHeight;
-//     }
-//     textFit(descrEls, {
-//         maxFontSize: 14
-//     });
-// }
-
 $(function () {
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
     });
-});
-
-resizeTextToFit();
-addEventListener("resize", () => {
-    resizeTextToFit();
 });
